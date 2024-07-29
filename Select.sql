74b95c1d0eba2b497023dbdfb08ad39324a9bb22
d8nd8 <jumppmuj@yandex.ru>
2024-07-29 18:13:18 +0300

    Reworked Select added

--- Select.sql
+++ Select.sql
@@ -4,7 +4,7 @@ WHERE length = (SELECT MAX(length) FROM songs);
 
 --2.2
 SELECT name,length FROM songs
-WHERE length > 210;
+WHERE length >= 210;
 
 --2.3
 SELECT name,releaseyear FROM compilations
@@ -16,7 +16,7 @@ WHERE name NOT LIKE '% %';
 
 --2.5
 SELECT name FROM songs
-WHERE name LIKE '%мой%' OR name LIKE '%my%';
+WHERE string_to_array(lower(name), ' ') && ARRAY['my','мой'];
 
 --3.1
 SELECT g.Name AS GenreName, COUNT(a.ID) AS ArtistCount
@@ -25,22 +25,31 @@ JOIN ArtistGenre ag ON g.ID = ag.GenreID
 JOIN Artists a ON ag.ArtistID = a.ID
 GROUP BY g.Name;
 
+--3.2
 SELECT COUNT(s.ID) AS TrackCount
 FROM Albums a
 JOIN Songs s ON a.ID = s.Album
 WHERE a.releaseyear BETWEEN 2019 AND 2020;
 
+--3.3
 SELECT a.Name AS AlbumNAme, AVG(s.length) AS AverageDuration
 FROM Albums a
 JOIN Songs s ON a.ID = s.Album
 GROUP BY a.ID, a.Name;
 
+--3.4
 SELECT a.Name AS ArtistName
 FROM Artists a
-LEFT JOIN ArtistAlbum aa ON a.ID = aa.ArtistID
-LEFT JOIN Albums al ON aa.AlbumID = al.ID AND al.ReleaseYear = 2020
-WHERE al.ID IS NULL;
+WHERE a.ID NOT IN (
+	SELECT DISTINCT a2.ID
+	FROM Artists a2
+	JOIN ArtistAlbum aa ON a2.ID = aa.ArtistID
+	JOIN Albums al ON aa.AlbumID = al.ID
+	WHERE al.releaseyear = 2020
+);
 
+
+--3.5
 SELECT DISTINCT c.Name AS CompilationTitle
 FROM Compilations c
 JOIN CompilationSong cs ON c.ID = cs.CompilationID
